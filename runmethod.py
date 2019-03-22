0afc9cd646d3124b99dd3117c5f4f1150db6674b
xushuhui <474497097@qq.com>
2019-03-22 10:28:30 +0800

    update

--- runmethod.py
+++ runmethod.py
@@ -4,16 +4,16 @@ import json
 from utils.use_json import UseJson
 class RunMethod:
     def post(self,url=None,data=None,header=None):
-        res = requests.post(url=url,data=data,headers=header,verify=False)
-        return res.json()
+        res = requests.post(url=url,json=data,headers=header,verify=False)
+        return self.handler(res)
     def get(self,url=None,data=None,header=None):
-        res = requests.get(url=url,data=data,headers=header,verify=False)
+        res = requests.get(url=url,json=data,headers=header,verify=False)
         return self.handler(res)
     def put(self,url=None,data=None,header=None):
-        res = requests.put(url=url,data=data,headers=header,verify=False)
+        res = requests.put(url=url,json=data,headers=header,verify=False)
         return self.handler(res)
     def delete(self,url=None,data=None,header=None):
-        res = requests.delete(url=url,data=data,headers=header,verify=False)
+        res = requests.delete(url=url,json=data,headers=header,verify=False)
         return self.handler(res)
     def handler(self,res):
         if res.status_code != 200:
@@ -28,5 +28,6 @@ if __name__ == '__main__':
     jsons = UseJson()
     client = RunMethod()
     data = jsons.get_data("createRoom")
-    data = json.dumps(data)
-    client.main("post")("https://dev.yunsuit.com/index",data)
+    # print(data)
+    # data = json.dumps(data)
+    client.main("post")("http://demo/index",data)
--- utils/use_json.py
+++ utils/use_json.py
@@ -4,17 +4,15 @@ class UseJson():
     def __init__(self):
         pass
     def read_data(self):
-        with open('./static/data.json') as fp:
+        with open('./static/data.json','r',encoding='utf-8') as fp:
             data = json.load(fp)
             return data
     def get_data(self,key):
         self.data = self.read_data()
         return self.data[key]
-    def get_json_data(self,key):
-        return json.dumps(self.get_data(key))
     def write_data(self,key,value=None):
         data = self.read_data()
-        with open('./static/data.json',"w+") as f:
+        with open('./static/data.json',"w+",encoding='utf-8') as f:
             data[key] = value
             return json.dump(data,f)
             
